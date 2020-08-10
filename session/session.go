047f68964bf57462f8ecbb9ffab6d020ef11f3b1
lysu <sulifx@gmail.com>
2020-08-10 19:53:19 +0800

    session:  init tidb_audit user

--- session/session.go
+++ session/session.go
@@ -1845,7 +1845,7 @@ func CreateSessionWithDomain(store kv.Storage, dom *domain.Domain) (*session, er
 
 const (
 	notBootstrapped         = 0
-	currentBootstrapVersion = version45
+	currentBootstrapVersion = version47
 )
 
 func getStoreBootstrapVersion(store kv.Storage) int64 {
